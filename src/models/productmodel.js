37f48269f71c4c33076806d1d761b7ae2e9c312d
unknown <mittalmeenakshi1996@gmail.com>
2022-08-25 15:42:56 +0530

    changed

---
+++ src/models/productmodel.js
@@ -0,0 +1,12 @@
+const mongoose = require ('mongoose');
+
+
+const productschema =new mongoose.Schema(
+
+{
+	name:String,
+	category:String,
+	price:{type:Number,
+		required:true}//mandatory property
+},{timestamps:true})
+module.exports=mongoose.model ('product',productschema)
