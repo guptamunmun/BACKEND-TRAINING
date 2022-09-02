dd29e4fecafdf8f76ebd210108f11f83a73a2f4f
unknown <mittalmeenakshi1996@gmail.com>
2022-09-02 16:42:51 +0530

    lodash functions

--- src/routes/route.js
+++ src/routes/route.js
@@ -18,7 +18,19 @@ router.get('/test-me', function (req, res) {
     const month =['jan','feb','march','april','may','june','july','aug','sept','oct','nov','dec']
     const splitmonth = lodash.chunk(month,4);
     console.log(splitmonth)
-    
+    const oddnumbers =[1,3,5,7,9,11,13,15,17,19]
+   const last9element=lodash.tail(oddnumbers);
+   console.log(last9element)
+   const arr1 =[1,2,4,1]
+   const arr2 =[3,0,6,4]
+   const arr3 =[1,4,3,2,5]
+   const arr4 =[3,2,1,5,6]
+   const arr5 =[3,4,2,6,9]
+   const mergearray =lodash.merge(arr1,arr2,arr3,arr4,arr5)
+   console.log(mergearray)
+   const obj = [ ["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+const mergedobj = lodash.fromPairs(obj)  
+console.log(mergedobj)  
     res.send('My second ever api!')
 });
 
